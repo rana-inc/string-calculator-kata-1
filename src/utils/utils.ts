57da86beaab08cf583f834059d2d40b24e181536
Rana Ankit <rana@incubyte.co>
2025-02-05 10:16:38 +0530

    update add logic to handle any number of inputs

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,12 @@
 export const add = (numbers: string): number => {
   if (numbers.length === 0) return 0;
-
   const numbersArr = numbers.split(",");
-  if (numbersArr.length > 1) {
-    return parseInt(numbersArr[0]) + parseInt(numbersArr[1]);
-  }
-  return parseInt(numbers);
+
+  if (numbersArr.length === 1) return parseInt(numbers);
+
+  const sum = numbersArr.reduce((prevValue, currentValue) => {
+    return prevValue + parseInt(currentValue);
+  }, 0);
+
+  return sum;
 };
