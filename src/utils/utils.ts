941a43c9c0487b23444be66ff7f84d3733a59781
Rana Ankit <rana@incubyte.co>
2025-02-05 10:28:54 +0530

    updated add function logic to handle new line between numbers

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,8 @@
 export const add = (numbers: string): number => {
   if (numbers.length === 0) return 0;
-  const numbersArr = numbers.split(",");
+
+  const regex = new RegExp(`,|\\n`);
+  const numbersArr = numbers.split(regex);
 
   const sum = numbersArr.reduce((prevValue, currentValue) => {
     return prevValue + parseInt(currentValue);
