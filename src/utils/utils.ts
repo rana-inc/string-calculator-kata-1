64d6e0f1f019992f895347c184ea5722a98037f5
Rana Ankit <rana@incubyte.co>
2025-02-05 10:17:16 +0530

    refactor , remove redundant code

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,8 +2,6 @@ export const add = (numbers: string): number => {
   if (numbers.length === 0) return 0;
   const numbersArr = numbers.split(",");
 
-  if (numbersArr.length === 1) return parseInt(numbers);
-
   const sum = numbersArr.reduce((prevValue, currentValue) => {
     return prevValue + parseInt(currentValue);
   }, 0);
