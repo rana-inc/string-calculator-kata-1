30dd92aa8f41d1f541b823112548c15986a0e539
Rana Ankit <rana@incubyte.co>
2025-02-05 10:52:37 +0530

    added new test case for new line scenario

--- __tests__/stringCalculator.test.tsx
+++ __tests__/stringCalculator.test.tsx
@@ -27,5 +27,6 @@ describe("String Calculator", () => {
 
   it("should able to handle new lines between numbers", () => {
     expect(add("1\n2,3")).toBe(6);
+    expect(add("1\n2\n100")).toBe(103);
   });
 });
