c13c36ffba21c2adb7f1010dfa3acb415563628c
Rana Ankit <rana@incubyte.co>
2025-02-05 09:32:31 +0530

    added intial scenario and first failing test for string calculator

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -1,1 +1,13 @@
-// 
+//Create a simple String calculator with a method signature:
+// ———————————————
+// int Add(string numbers)
+// ———————————————
+// The method can take up to two numbers, separated by commas, and will return their sum.
+// for example “” or “1” or “1,2” as inputs.
+// (for an empty string it will return 0)
+
+describe("String Calculator", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add("")).toEqual(0);
+  });
+});
