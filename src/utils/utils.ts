b63ea51879e639f3810404d46d99c8977f519829
Rana Ankit <rana@incubyte.co>
2025-02-05 10:05:53 +0530

    update logic of add function to handle two number input

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,9 @@
 export const add = (numbers: string): number => {
   if (numbers.length === 0) return 0;
 
+  const numbersArr = numbers.split(",");
+  if (numbersArr.length > 1) {
+    return parseInt(numbersArr[0]) + parseInt(numbersArr[1]);
+  }
   return parseInt(numbers);
 };
